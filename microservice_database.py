2cfa2d49e1c15f523e7799b1f2d5e81d35482edf
student <shailavishah17@csu.fullerton.edu>
2019-03-13 01:12:54 -0700

    updated userservice file

--- microservice_database.py
+++ microservice_database.py
@@ -1,8 +1,15 @@
 import sqlite3
 connection = sqlite3.connect('microservice.db')
 c=connection.cursor()
-c.execute("""create table if not exists users_table(username TEXT,
-                                    useremail TEXT PRIMARY KEY,
+c.execute("""create table if not exists users_table(name TEXT,
+                                    username TEXT PRIMARY KEY,
                                     userpassword TEXT)""")
+c.execute("""create table if not exists articles_table (article_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                            article_title text,
+                                            article_content text,
+                                            article_author text,
+                                            FOREIGN KEY(article_author) REFERENCES users_table(username)
+                                            createdDate text,
+                                            modifiedDate text)""")                                        
 connection.commit()
 connection.close()
--- userService.py
+++ userService.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, make_response,g
+from flask import Flask, jsonify, request, make_response,g,Response
 import sqlite3
 import re
 from passlib.hash import sha256_crypt
@@ -29,67 +29,51 @@ def verify(username, password):
     db = get_database()
     c = db.cursor()
     try:
-        c.execute("select userpassword from users_table where useremail=(:email)", {'email':username})
+        c.execute("select userpassword from users_table where username=(:email)", {'email':username})
         row = c.fetchone()
         if row is not None:
-            print("Inside If")
             p = row[0]
-            print(p)
         else:
             return False
 
         if (sha256_crypt.verify(password,p)):
             return True
-        '''
-        c.execute("select * from users where email=(:email) and password=(:password)",{'email':username,'password':password})
-        n = c.fetchone()[0]
 
-        if (n == 1):
-            return True
-        '''
     except sqlite3.Error as er:
         print(er)
 
     return False
 
-
 @app.route("/createuser", methods=['POST'])
 def createuser():
     if (request.method == 'POST'):
-        print("Inside Post method")
-        user_details = request.get_json()
-        print(user_details)
-        if not user_details['name']:
-            print("Inside If")
-            response_message = 'Please enter your name'
-        elif not user_details['username'] or not re.match(r"[^@]+@[^@]+\.[^@]+", user_details['username']):
-            print("Inside elif")
-            response_message = 'Please enter your valid email address'
-        elif not user_details['userpassword']:
-            response_message = 'Please enter your password'
+        if not request.json.get('name'):
+            return jsonify("Please enter your name")
+        elif not request.json.get('username'):
+            return jsonify("Please enter email address")
+        elif not re.match(r"[^@]+@[^@]+\.[^@]+", user_details['username']):
+            return jsonify("Please enter valid email address")
+        elif not request.json.get('userpassword'):
+            return jsonify("Please enter your password")
         else:
+            user_details = request.get_json()
             user_password = sha256_crypt.encrypt((str(user_details['userpassword'])))
             db = get_database()
             try:
-                db.cursor().execute("insert into users_table (username, useremail, userpassword) values (?,?,?)",
+                db.cursor().execute("insert into users_table (name, username, userpassword) values (?,?,?)",
                     [user_details['name'], user_details['username'], user_password])
                 db.commit()
-                response_message = {
-                    'status': 201,
-                    'message': 'Created: ' + request.url,
-                }
+                response_message = Response(status=201, mimetype='application/json')
+                response_message.headers['location'] = 'https://127.0.0.1:5000/createuser'
+                return response_message
             except sqlite3.Error as e:
                 print(e)
-                return make_response(jsonify({'Error Message': 'Conflict'}), 409)
-
-        #for row in db.cursor().execute("select * from users_table"):
-            #print(row)
+                response_message = Response(status=409, mimetype='application/json')
+                return response_message
     else:
         return jsonify("enter valid details")
-    response = jsonify(response_message)
-    return make_response(response)
 
-@app.route("/display", methods=['POST'])
+@app.route("/display", methods=['GET'])
 def display():
     db = get_database()
     c = db.cursor()
@@ -102,89 +86,39 @@ def display():
     }
     return jsonify(message)
 
-@app.route("/deleteuser", methods=['POST'])
+@app.route("/deleteuser", methods=['DELETE'])
 @auth.login_required
 def deleteuser():
     db = get_database()
     c = db.cursor()
-    print("Inside delete user function")
-    user_details = request.get_json()
-    message = {}
+    username = request.authorization.username
     try:
-        c.execute("select userpassword from users_table where useremail=(:email)", {'email':user_details['useremail']})
-        row = c.fetchone()
-        if row is not None:
-            print("Inside If")
-            p = row[0]
-            print(p)
-            if(sha256_crypt.verify(user_details['userpassword'],p)):
-                print("Inside If")
-                c.execute("delete from users_table where useremail=(:email)",{'email':user_details['useremail']})
-                db.commit()
-                print("Record deleted")
-                message = {
-                    'status': 201,
-                    'mesg': 'Deleted: ' + request.url,
-                }
-            else:
-                message = {
-                    'status': 201,
-                    'mesg': 'Password does not match: ' + request.url,
-                }
-        else:
-            message = {
-                'status': 201,
-                'mesg': 'User does not match: ' + request.url,
-            }
-
+        c.execute("delete from users_table where username=(:email)",{'email':username})
+        db.commit()
+        response_message = Response(status=200, mimetype='application/json')
+        return response_message
     except sqlite3.Error as er:
-            print(er)
+        print(er)
+        response_message = Response(status=409, mimetype='application/json')
+        return response_message
 
-    return jsonify(message)
-
-@app.route("/updatepassword", methods=['POST'])
+@app.route("/updatepassword", methods=['PATCH'])
 @auth.login_required
 def updatepassword():
     db = get_database()
     c = db.cursor()
-    message = {}
-    print("inside update password")
+    username = request.authorization.username
     user_details = request.get_json()
     new_password = sha256_crypt.encrypt((str(user_details['new_password'])))
-    #print(details['name'])
-    print(user_details['useremail'])
-    #print(details['password'])
-    #print(pas)
     try:
-        c.execute("select userpassword from users_table where useremail=(:email)", {'email':user_details['useremail']})
-        row = c.fetchone()
-        if row is not None:
-            p = row[0]
-            print(p)
-            if(sha256_crypt.verify(user_details['userpassword'],p)):
-                print("Inside If")
-                c.execute("update users_table set userpassword = (:password) where useremail=(:email)",{'email':user_details['useremail'], 'password':new_password})
-                db.commit()
-                print("password updated")
-                message = {
-                    'status': 201,
-                    'mesg': 'Password Updated: ' + request.url,
-                }
-            else:
-                message = {
-                    'status': 201,
-                    'mesg': 'Password does not match: ' + request.url,
-                }
-        else:
-            message = {
-                'status': 201,
-                'mesg': 'User does not match: ' + request.url,
-            }
-
+        c.execute("update users_table set userpassword = (:password) where username=(:email)",{'email':username,'password':new_password})
+        db.commit()
+        response_message = Response(status=200, mimetype='application/json')
+        return response_message
     except sqlite3.Error as er:
         print(er)
-
-    return jsonify(message)
+        response_message = Response(status=409, mimetype='application/json')
+        return response_message
 
 if __name__ == '__main__':
     app.run(debug=True)
